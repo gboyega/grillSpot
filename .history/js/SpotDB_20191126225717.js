bebb9f6526cdb8a7ce34dbe83ed84e9cfd7ee606
gboyega <jaiyeolagboyega@gmail.com>
2019-11-29 17:16:35 +0100

    created file structure and created index page

---
+++ .history/js/SpotDB_20191126225717.js
@@ -0,0 +1,116 @@
+function SpotsDb
+    let spots = [
+        {
+            name: 'Asun-spot',
+            location: 'Lekki',
+            Operator: "Adamu Danladi",
+            address: "15 Caulcrick drive, Lekki phase 1, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@aspot.com',
+            web: 'http://aspot.com',
+            img:'',
+            menu: [{ name: 'Asun', price: '1500' }, 
+                    { name: 'Grilled-fish and chips', price: '2500' }, 
+                    { name: 'Hot-sauce', price: '500' } 
+                ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img:'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }],
+            category:"Asun"
+        },
+
+        {
+            name: 'Suya-spot',
+            location: 'Lekki',
+            Operator: "Adekunle Alabi",
+            address: "15 Joker drive, Lekki phase 1, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@sspot.com',
+            web: 'http://sspot.com',
+            img: '',
+            menu: [{ name: 'Suya', price: '1500' },
+            { name: 'Grilled-fish and chips', price: '2500' },
+            { name: 'Hot-sauce', price: '500' }
+            ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },,
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }
+            ],
+            category:'Suya'
+        },
+
+        {
+            name: 'Asun-spot',
+            location: 'Ikeja',
+            Operator: "Adamu Danladi",
+            address: "15 Caulcrick drive, Ikeja-GRA, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@aspot.com',
+            web: 'http://aspot.com',
+            img: '',
+            menu: [{ name: 'Asun', price: '1500' },
+            { name: 'Grilled-fish and chips', price: '2500' },
+            { name: 'Hot-sauce', price: '500' }
+            ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }],
+            category: "Asun"
+        },
+
+        {
+            name: 'Suya-spot',
+            location: 'Ikeja',
+            Operator: "Adekunle Alabi",
+            address: "15 Joker drive, Ikeja-GRA, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@sspot.com',
+            web: 'http://sspot.com',
+            img: '',
+            menu: [{ name: 'Suya', price: '1500' },
+            { name: 'Grilled-fish and chips', price: '2500' },
+            { name: 'Hot-sauce', price: '500' }
+            ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }
+            ],
+            category: 'Suya'
+        },
+
+        {
+            name: 'Asun-spot',
+            location: 'Ikoyi',
+            Operator: "Adamu Danladi",
+            address: "15 Caulcrick drive, Ikoyi 1, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@aspot.com',
+            web: 'http://aspot.com',
+            img: '',
+            menu: [{ name: 'Asun', price: '1500' },
+            { name: 'Grilled-fish and chips', price: '2500' },
+            { name: 'Hot-sauce', price: '500' }
+            ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }],
+            category: "Asun"
+        },
+
+        {
+            name: 'Suya-spot',
+            location: 'Ikoyi',
+            Operator: "Adekunle Alabi",
+            address: "15 Joker drive, Ikoyi, Lagos",
+            phone: '+2348070000101',
+            mail: 'danladi@sspot.com',
+            web: 'http://sspot.com',
+            img: '',
+            menu: [{ name: 'Suya', price: '1500' },
+            { name: 'Grilled-fish and chips', price: '2500' },
+            { name: 'Hot-sauce', price: '500' }
+            ],
+            review: [{ name: 'Ade Micheal', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/steve.jpg' },
+            { name: 'Ruth Grace', review: 'Tasty fresh Asun and unbelievable serving size', rating: '5', img: 'https://react.semantic-ui.com/images/avatar/large/molly.png' }
+            ],
+            category: 'Suya'
+        },
+    ]
+
+    return spots;
+}
