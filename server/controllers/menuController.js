066abbad1299e54f5708f3ade1a942c3b9ea6d0e
gboyega <jaiyeolagboyega@gmail.com>
2020-02-05 04:53:26 +0100

    added menu controller

--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -10,8 +10,40 @@ module.exports.create = (req, res) => {
     .catch(err => res.status(400).send({error:err, message: 'Menu item not created. Please try again.'}));
 };
 
-module.exports.getOne = (req, res) => {
-    
+module.exports.getMany = async (req, res) => {
+  const menu = await Menu.find({ spotId: req.params.spotId });
+  try {
+    if (reviews.length == 0) {
+      () =>
+        res
+          .status(204)
+          .json({ message: "No menu available for this spot" });
+    } else {
+      () => res.status(200).send(menu);
+    }
+  } catch (err) {
+    console.log(err);
+    () => res.status(500).send();
+  }
 };
 
-module.exports.update = () => {};
+
+module.exports.update = (req, res) => {
+  const menu = req.body;
+
+  Todo.findByIdAndUpdate({ _id: req.params.menuId }, menu, { new: true })
+    .then(menu =>
+      res.json({ body: menu, message: "Todo updated successfully" })
+    )
+    .catch(err =>
+      res.status(400).json({ error: err, message: "cannot update Menu" })
+    );
+};
+
+module.exports.delete = (req, res) => {
+  Todo.findOneAndDelete({ _id: req.params.menuId })
+    .then(() => res.json({ message: "Menu item has been deleted" }))
+    .catch(err =>
+      res.status(400).json({ error: err, message: "cannot delete Menu item" })
+    );
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,43 +1,41 @@
-var multer = require('multer');
+var multer = require("multer");
 var upload = multer({ dest: "temp/", limits: { fieldSize: 8 * 1024 * 1024 } });
-const spotResource = require('../controllers/spotController');
+const spotResource = require("../controllers/spotController");
 
-const reviewResource = require('../controllers/reviewController');
+const reviewResource = require("../controllers/reviewController");
 
-const userResource = require('../controllers/userController');
+const userResource = require("../controllers/userController");
 
-const menuResource = require('../controllers/menuController');
+const menuResource = require("../controllers/menuController");
 
-module.exports = (app) => {
-    app.route('/api/spots/')
+module.exports = app => {
+  app
+    .route("/api/spots/")
     .post(spotResource.create)
     .get(spotResource.getAll);
 
-    app.route("/api/upload/").post(upload.single("file"), spotResource.upload);
+  app.route("/api/upload/").post(upload.single("file"), spotResource.upload);
 
-    app.route('/api/spots/:spotId')
+  app
+    .route("/api/spots/:spotId")
     .get(spotResource.getOne)
     .put(spotResource.update)
     .delete(spotResource.delete);
 
-    app.route('/api/reviews/')
-    .post(reviewResource.create);
+  app.route("/api/reviews/").post(reviewResource.create);
 
-    app.route('/api/reviews/:spotId')
-    .get(reviewResource.getMany);
+  app.route("/api/reviews/:spotId").get(reviewResource.getMany);
 
-    app.route('/api/users/')
-    .post(userResource.create);
+  app.route("/api/users/").post(userResource.create);
 
-    app.route('/api/users/login')
-    .post(userResource.login);
+  app.route("/api/users/login").post(userResource.login);
 
-    app.route('/api/menu/')
-    .post(menuResource.create);
+  app.route("/api/menu/").post(menuResource.create);
 
-    app.route('/api/menu/:spotId')
-    .get(menuResource.getOne)
-    .put(menuResource.update);
+  app.route("/api/menu/:spotId").get(menuResource.getMany);
 
-
+  app
+    .route("/api/menu/:menuId")
+    .put(menuResource.update)
+    .delete(menuResource.delete);
 };
